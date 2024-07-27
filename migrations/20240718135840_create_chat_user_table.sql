3850b9c312c33027bff45dc1b6a70c259c05ed42
DenisCom3 <denis.chernov.and@gmail.com>
2024-07-27 12:56:46 +0400

    fix: update migrations

--- migrations/20240718135840_create_chat_user_table.sql
+++ migrations/20240718135840_create_chat_user_table.sql
@@ -1,8 +1,8 @@
 -- +goose Up
 -- +goose StatementBegin
 create table IF NOT EXISTS chat_users (
-    chat_id                 BIGSERIAL REFERENCES chats(id) NOT NULL,
-    user_id                 BIGSERIAL REFERENCES users(id) NOT NULL
+    chat_id                 BIGSERIAL REFERENCES chats(id) NOT NULL ON DELETE CASCADE,
+    user_id                 BIGSERIAL NOT NULL
 );
 -- +goose StatementEnd
 
--- migrations/20240718135856_create_messages_table.sql
+++ migrations/20240718135856_create_messages_table.sql
@@ -3,9 +3,10 @@
 create table IF NOT EXISTS messages (
     id                      BIGSERIAL PRIMARY KEY,
     chat_id                 BIGSERIAL REFERENCES chats(id) NOT NULL,
-    user_id                 BIGSERIAL REFERENCES users(id) NOT NULL,
-    message                 VARCHAR(300) NOT NULL,
-    created_at               TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+    user_id                 BIGSERIAL REFERENCES chat_users(user_id) NOT NULL,
+    text                    VARCHAR(300) NOT NULL,
+    created_at              TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+    updated_at              TIMESTAMP 
 );
 -- +goose StatementEnd
 
